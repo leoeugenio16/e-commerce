4af19320bed18a551023ea6c4736fbf09ca18b0b
leoeugenio16 <leoeugenio16@gmail.com>
2024-10-22 17:30:45 -0300

    se modifica linea para descargar archiuvos de internet

--- e-commerce_PCHub/init-backend.bat
+++ e-commerce_PCHub/init-backend.bat
@@ -4,7 +4,7 @@
 psql -U postgres -d postgres -c "CREATE DATABASE ecommerce-PCHUB" || echo "Base de datos ya existe"
 
 :: Descargar backup de base de datos desde Google Drive
-wget https://drive.google.com/file/d/162-FQeIojn5B-m9ZeMpTNPxkv_lAkA9M/view?usp=sharing -O db_backup.sql
+curl -o db_backup.sql https://drive.google.com/uc?id=162-FQeIojn5B-m9ZeMpTNPxkv_lAkA9M
 
 :: Restaurar backup de base de datos
 psql -U postgres -d ecommerce-PCHUB < db_backup.sql
